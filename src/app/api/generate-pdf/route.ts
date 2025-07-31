0060972c34ad61ca10840eb50caf3c9bbf6d442a
macos <nikhil.tekawade@evonix.co>
2025-07-31 14:07:17 +0530

    Nikhil: template change for pdf

--- src/app/api/generate-pdf/route.ts
+++ src/app/api/generate-pdf/route.ts
@@ -2,32 +2,32 @@ import path from 'path';
 import fs from 'fs/promises';
 import { NextResponse } from 'next/server';
 import chromium from '@sparticuz/chromium';
-import puppeteer from 'puppeteer-core';import handlebars from 'handlebars';
+import puppeteer from 'puppeteer-core'; import handlebars from 'handlebars';
 import { writeFileSync } from 'fs';
 import { supabase } from '../../../../utils/supabaseClient';
 
 export async function POST(req: Request) {
-    try{
-  const data = await req.json(); // example: { name: 'Nikhil', email: '...', amount: '...' }
-  const { reference_id, customer_name, phone_no, battery_serial_no,pin_code,city,
-        purchase_dat,retailer_shop_name,warranty_start_date,warranty_end_date,request_date,
-    varroc_part_code,manufacturing_date,description } = data;
+  try {
+    const data = await req.json(); // example: { name: 'Nikhil', email: '...', amount: '...' }
+    const { reference_id, customer_name, phone_no, battery_serial_no, pin_code, city,
+      purchase_dat, retailer_shop_name, warranty_start_date, warranty_end_date, request_date,
+      varroc_part_code, manufacturing_date, description } = data;
     data.logo_base64 = await fetchImageAsBase64('https://varroc-chatbot-final.vercel.app/images/varroc.png');
     data.hiker_logo_base64 = await fetchImageAsBase64('https://varroc-chatbot-final.vercel.app/images/hiker.png');
 
-  console.log("whole path got in process.cwd()",process.cwd());
-        
-  const templatePath = path.join(process.cwd(), 'htmltemplates', 'warrantycertificate.html');
-  const html = await fs.readFile(templatePath, 'utf8');
+    console.log("whole path got in process.cwd()", process.cwd());
+
+    const templatePath = path.join(process.cwd(), 'htmltemplates', 'warrantycertificate.html');
+    const html = await fs.readFile(templatePath, 'utf8');
     const compiledTemplate = handlebars.compile(html);
     const finalHtml = compiledTemplate(data);
 
     const browser = await puppeteer.launch({
-  args: chromium.args,
-  defaultViewport: { width: 1280, height: 800 }, // ✅ set it here
-  executablePath: await chromium.executablePath(),
-  headless: true,
-});
+      args: chromium.args,
+      defaultViewport: { width: 1280, height: 800 }, // ✅ set it here
+      executablePath: await chromium.executablePath(),
+      headless: true,
+    });
 
     const page = await browser.newPage();
     await page.setContent(finalHtml, { waitUntil: 'domcontentloaded' });
@@ -50,22 +50,22 @@ export async function POST(req: Request) {
     // // Construct URL
     // const fileURL = `${process.env.NEXT_PUBLIC_BASE_URL}/api/loadImagePath?imagePath=/certificates/generated/${fileName}`;
     // console.log("this is the fileurl on upload",fileURL);
-    const supabasefileName=fileName.replaceAll("-","_")
+    const supabasefileName = fileName.replaceAll("-", "_")
     let bucket = "complaint";
-              const contentType = 'application/pdf';
-    
-              const { data:supabaseurl, error } = await supabase.storage
-                .from(bucket)
-                .upload(supabasefileName, pdfBuffer, {
-                  contentType,
-                  upsert: true,
-                });
-    
-              if (error) {
-                console.error("Upload error:", error);
-              }
-              const { data: publicUrlData } = supabase.storage.from(bucket).getPublicUrl(supabasefileName);
-    
+    const contentType = 'application/pdf';
+
+    const { data: supabaseurl, error } = await supabase.storage
+      .from(bucket)
+      .upload(supabasefileName, pdfBuffer, {
+        contentType,
+        upsert: true,
+      });
+
+    if (error) {
+      console.error("Upload error:", error);
+    }
+    const { data: publicUrlData } = supabase.storage.from(bucket).getPublicUrl(supabasefileName);
+
     return NextResponse.json({ url: publicUrlData.publicUrl });
   } catch (err) {
     console.error('PDF Generation Error:', err);
